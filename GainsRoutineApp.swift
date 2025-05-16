8e801699ac4ecc957c24aa24a4cbedf212978c7d
arya <68519362+aryasomu@users.noreply.github.com>
2025-05-16 15:14:53 -0700

    Add files via upload

---
+++ GainsRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainsRoutineApp.swift
+//  GainsRoutine
+//
+//  Created by Arya Somu on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GainsRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
